f4634f1cb358855fb3a102f1bfcf5e97a634b839
sasikalachinnasamy <sasikala.chinnasamy@gmail.com>
2021-09-16 20:30:53 +0530

    websocket connection

--- deriv_api/deriv_api.py
+++ deriv_api/deriv_api.py
@@ -1,7 +1,29 @@
 #
-def hello() -> str:
-    """
-    A dummy function
-    """
+#import websocket
+class deriv_api:
+    connection = ''
+    lang = 'EN'
+    brand = 'binary'
 
-    return "Hello world!"
+    def __init__(self, app_id, connection, endPoint = 'frontend.binaryws.com', lang = 'EN', brand = '') -> None:
+        if connection :
+            self.connection = connection
+        else:
+            self.endPoint = endPoint
+            self.lang = lang
+            self.brand = brand
+            self.__connect()
+        pass
+
+    def __connect(self) -> None:
+        #apiUrl = "wss://ws.binaryws.com/websockets/v3?app_id="+self.app_id
+        #ws = websocket.WebSocketApp(apiUrl, on_message=on_message, on_open=on_open)
+        #ws.run_forever()
+        pass
+
+    def hello(self) -> str:
+        """
+        A dummy function
+        """
+
+        return "Hello world!"
