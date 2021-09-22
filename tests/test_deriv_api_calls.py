70550cb9172ce17b32ed6a7a5d3d6e4a21879b76
chylli-binary <chylli@binary.com>
2021-09-22 09:36:33 +0000

    mock a test

--- tests/test_deriv_api_calls.py
+++ tests/test_deriv_api_calls.py
@@ -3,7 +3,10 @@ import pytest
 from deriv_api import deriv_api_calls
 
 @pytest.mark.asyncio
-async def test_deriv_api_calls():
+async def test_deriv_api_calls(mocker):
     api = deriv_api_calls.DerivAPICalls()
     assert(isinstance(api, deriv_api_calls.DerivAPICalls))
     assert (await api.accountClosure("hello"))['needsMethodArg'] == '1'
+    mocker.patch('deriv_api.deriv_api_calls.validateArgs', return_value='bad' )
+    with pytest.raises(ValueError, match='bad'):
+        await api.accountClosure("hello")
