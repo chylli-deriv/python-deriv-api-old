473a92666a97f7f8250aad4bbf9f6af5f8bd5ae8
chylli-binary <chylli@binary.com>
2021-10-03 23:28:06 +0800

    why subscribe will break the whole system?

--- study/subject2.py
+++ study/subject2.py
@@ -4,20 +4,22 @@ from rx import operators as op
 import asyncio
 loop = asyncio.get_event_loop()
 def get_source():
-    subject = Subject()
-    subject.on_next("hello")
+    #subject = Subject()
+    #subject.on_next(1)
+    subject = rx.defer(lambda i: rx.of(1,2,3)).pipe(op.share())
     return subject
 def forget_old_source():
     return
 
 def func_test():
-    source = get_source().pipe(op.finally_action(forget_old_source), op.share())
+    source = get_source().pipe(op.share())
+    #source.subscribe(lambda d: print(f"get {d}"))
     async def process_response():
         response = None
         try:
             print("in try before wait")
             response = await source.pipe(op.first(), op.to_future(loop.create_future))
-            print("in try after await")
+            print(f"in try after await {response}")
         except Exception as err:
             print(f"error {err}")
     task = loop.create_task(process_response())
@@ -25,3 +27,10 @@ def func_test():
     return source
 
 func_test()
+#source = get_source()
+#def myof(i):
+#    print(f"get {i} in defer")
+#    return rx.of(1,2,3)
+#source = rx.defer(myof)
+#print(type(source))
+#source.subscribe(lambda i: print(f"get {i}"))
