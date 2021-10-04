3efc05cbcecf311b53c1fba6a0d9096a335bdf13
chylli-binary <chylli@binary.com>
2021-10-04 13:59:13 +0800

    test 6

--- study/subject6.py
+++ study/subject6.py
@@ -3,8 +3,11 @@ from rx.subject import Subject
 from rx import operators as op
 import asyncio
 subject = Subject()
+
 def get_source():
+    subject.subscribe(lambda i: print(f"get {i} in get_source before share "))
     source = subject.pipe(op.share())
+    source.subscribe(lambda i: print(f"get {i} in get_source"))
     return source
 def forget_old_source():
     return
@@ -15,12 +18,16 @@ async def func_test():
     response = None
     try:
         print("in try before wait")
-        response = await source.pipe(op.first(), op.to_future())
+        #response = await source.pipe(op.first(), op.to_future())
+        source.pipe(op.first()).subscribe(lambda i:print(f"get in first {i}"))#, op.to_future())
         print(f"in try after await {response}")
     except Exception as err:
         print(f"error {err}")
     return source
 
+loop = asyncio.get_event_loop()
+task = loop.create_task(func_test())
 subject.on_next(1)
 subject.on_next(2)
-asyncio.run(func_test())
+loop.run_until_complete(task)
+#asyncio.run(task)
