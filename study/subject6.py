2fc4385a460ef70c4da0fb81d5920f4344257d95
chylli-binary <chylli@binary.com>
2021-10-04 14:54:16 +0800

    show subscriptions that created at first

--- study/subject6.py
+++ study/subject6.py
@@ -3,7 +3,7 @@ from rx.subject import Subject
 from rx import operators as op
 import asyncio
 subject = Subject()
-
+subject.subscribe(lambda i: print(f"get {i} after subject created"))
 def get_source():
     subject.subscribe(lambda i: print(f"get {i} in get_source before share "))
     source = subject.pipe(op.share())
@@ -18,8 +18,8 @@ async def func_test():
     response = None
     try:
         print("in try before wait")
-        response = await source.pipe(op.first(), op.to_future())
-        #source.pipe(op.first()).subscribe(lambda i:print(f"get in first {i}"))#, op.to_future())
+        #response = await source.pipe(op.first(), op.to_future())
+        source.pipe(op.first()).subscribe(lambda i:print(f"get in first {i}"))#, op.to_future())
         print(f"in try after await {response}")
     except Exception as err:
         print(f"error {err}")
