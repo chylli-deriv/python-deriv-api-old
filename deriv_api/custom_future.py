e6a2f592570f4bc8886bd78e64031dbf7b7cd385
chylli-binary <chylli@binary.com>
2021-10-09 06:39:52 +0800

    test ok

--- deriv_api/custom_future.py
+++ deriv_api/custom_future.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from asyncio import Future
 from asyncio import CancelledError
+from typing import Optional
+
 class CustomFuture(Future):
     def __init__(self, *, loop=None):
         self.state = 'pending'
@@ -47,3 +49,34 @@ class CustomFuture(Future):
 
     def is_cancelled(self) -> bool:
         return self.cancelled()
+
+    def then(self, then_callback, else_callback=None) -> CustomFuture:
+        new_future = CustomFuture(loop=self.get_loop())
+        def done_callback(myself: CustomFuture):
+            f: Optional[CustomFuture] = None
+            if myself.is_cancelled():
+                new_future.cancel('Upstream future cancelled')
+                return
+
+            if myself.is_rejected() and else_callback:
+                    f = else_callback(myself.exception())
+            elif myself.is_resolved() and then_callback:
+                f = then_callback(myself.result())
+
+            if f is None:
+                return
+
+            def inside_callback(internal_future: CustomFuture):
+                if internal_future.is_cancelled():
+                    new_future.cancel('Callback future cancelled')
+                    return
+                if internal_future.is_rejected():
+                    new_future.set_exception(internal_future.exception())
+                    return
+                if internal_future.is_resolved():
+                    new_future.set_result(internal_future.result())
+
+            f.add_done_callback(inside_callback)
+
+        self.add_done_callback(done_callback)
+        return new_future
--- tests/test_custom_future.py
+++ tests/test_custom_future.py
@@ -45,3 +45,83 @@ async def test_wrap():
         await f2
     assert f2.done()
     assert f2.is_cancelled()
+
+@pytest.mark.asyncio
+async def test_future_then():
+    # test upstream ok
+    # test callback future ok
+    f1 = CustomFuture()
+    def then_callback(last_result):
+        f = CustomFuture()
+        f.set_result(f"result: {last_result}")
+        return f
+    f2 = f1.then(then_callback)
+    f1.set_result("f1 ok")
+    assert (await f2) == 'result: f1 ok', "if inside future has result, then_future will has result too"
+
+    # test callback fail
+    f1 = CustomFuture()
+    def then_callback(last_result):
+        f = CustomFuture()
+        f.set_exception(Exception(f"result: {last_result}"))
+        return f
+
+    f2 = f1.then(then_callback)
+    f1.set_result("f1 ok")
+    with pytest.raises(Exception, match='result: f1 ok'):
+        await f2
+
+    # test callback fail
+    f1 = CustomFuture()
+
+    def then_callback(last_result):
+        f = CustomFuture()
+        f.set_exception(Exception(f"result: {last_result}"))
+        return f
+
+    f2 = f1.then(then_callback)
+    f1.set_result("f1 ok")
+    with pytest.raises(Exception, match='result: f1 ok'):
+        await f2
+
+    # test upstream fail
+    # test inside future ok
+    f1 = CustomFuture()
+    result = None
+
+    def else_callback(last_exception: Exception):
+        f = CustomFuture()
+        f.set_result(f"f1 exception {last_exception.args[0]}")
+        return f
+
+    f2 = f1.then(None, else_callback)
+    f1.set_exception(Exception("f1 bad"))
+    assert (await f2) == 'f1 exception f1 bad'
+
+    # test inside future exception
+    f1 = CustomFuture()
+    result = None
+
+    def else_callback(last_exception: Exception):
+        f = CustomFuture()
+        f.set_exception(Exception(f"f1 exception {last_exception.args[0]}"))
+        return f
+
+    f2 = f1.then(None, else_callback)
+    f1.set_exception(Exception("f1 bad"))
+    with pytest.raises(Exception, match='f1 exception f1 bad'):
+        await f2
+
+    # upstream canceled
+    f1 = CustomFuture()
+
+    def else_callback(last_exception: Exception):
+        f = CustomFuture()
+        f.set_exception(Exception(f"f1 exception {last_exception.args[0]}"))
+        return f
+
+    f2 = f1.then(None, else_callback)
+    f1.cancel('f1 cancelled')
+    with pytest.raises(asyncio.exceptions.CancelledError, match='Upstream future cancelled'):
+        await f2
+
